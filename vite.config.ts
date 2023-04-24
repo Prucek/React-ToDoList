e636db60c3b43b4d8e40f5ab31b2b6c52debb841
Prucek <prucek@redhat.com>
2023-04-24 22:45:21 +0200

    Added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()]
+	plugins: [
+		react(),
+		svgr(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['to-do.svg', 'to-do.png'],
+			manifest: {
+				short_name: 'TODO App',
+				name: 'TODO App',
+				description: 'A simple TODO App',
+				theme_color: '#8c1aff',
+				background_color: 'dark',
+				icons: [
+					{
+						src: '/to-do.svg',
+						type: 'image/svg+xml',
+						sizes: '48x48 192x192 512x512'
+					}
+				]
+			}
+		})
+	]
 });
