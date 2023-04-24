152888fa1d50cfd68b7498ae5536ac942021fb7c
Prucek <prucek@redhat.com>
2023-04-24 22:32:41 +0200

    Added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,118 @@
-import './App.css';
-import { ReactComponent as ReactLogo } from './assets/react.svg';
+import {
+	AppBar,
+	Container,
+	CssBaseline,
+	ThemeProvider,
+	Toolbar
+} from '@mui/material';
+// import './App.css';
+import {
+	Outlet,
+	RootRoute,
+	Route,
+	Router,
+	RouterProvider
+} from '@tanstack/react-router';
 
-const App = () => (
-	<div className="App">
-		<div>
-			<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-				<img src="/vite.svg" className="logo" alt="Vite logo" />
-			</a>
-			<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-				<ReactLogo className="logo react" title="React logo" />
-			</a>
-		</div>
-		<h1>ToDoList</h1>
-	</div>
-);
+import { ReactComponent as ReactLogo } from './assets/react.svg';
+import ButtonLink from './components/ButtonLink';
+import Home from './pages/Home';
+import About from './pages/About';
+import Reviews from './pages/Reviews';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import theme from './theme';
+
+const rootRoute = new RootRoute({
+	component: () => (
+		// const user = useLoggedInUser();
+		// const t = useTranslation();
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+
+			<AppBar sx={{ position: 'sticky' }}>
+				<Container maxWidth="sm">
+					<Toolbar disableGutters sx={{ gap: 2 }}>
+						<ButtonLink to="/">Home</ButtonLink>
+						<ButtonLink to="/about">About</ButtonLink>
+						<ButtonLink to="/reviews">Reviews</ButtonLink>
+						<ButtonLink to="/login">Login</ButtonLink>
+						{/* <Box sx={{ flexGrow: 1 }} />
+						{!user ? (
+							<ButtonLink to="/login">Login</ButtonLink>
+						) : (
+							<Button onClick={signOut}>Logout</Button>
+						)} */}
+					</Toolbar>
+				</Container>
+			</AppBar>
+
+			<Container
+				maxWidth="sm"
+				component="main"
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexGrow: 1,
+					gap: 2,
+					my: 4
+				}}
+			>
+				<Outlet />
+			</Container>
+		</ThemeProvider>
+	)
+});
+
+const indexRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: '/',
+	component: Home
+});
+
+const aboutRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: '/about',
+	component: About
+});
+
+const reviewsRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: '/reviews',
+	component: Reviews
+});
+
+const loginRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: '/login',
+	component: Login
+});
+
+const notFoundRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: '*',
+	component: NotFound
+});
+
+const routeTree = rootRoute.addChildren([
+	indexRoute,
+	aboutRoute,
+	reviewsRoute,
+	loginRoute,
+	notFoundRoute
+]);
+
+const router = new Router({ routeTree });
+
+declare module '@tanstack/react-router' {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
