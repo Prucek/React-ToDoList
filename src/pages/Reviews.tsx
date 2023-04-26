5c9ab695bffdcd01bef1bf9efaad6cc4f29ff7a8
Marek Miček <marek.micek@azet.sk>
2023-04-26 10:48:24 +0200

    Updated review page

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -1,2 +1,49 @@
-const Reviews = () => <>Reviews page</>;
+import { FC, useEffect, useState } from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { onSnapshot } from 'firebase/firestore';
+
+import ReviewPreview from '../components/ReviewPreview';
+import { reviewsCollection, Review } from '../firebase';
+import AddReview from '../components/AddReview';
+import useLoggedInUser from '../hooks/useLoggedInUser';
+import usePageTitle from '../hooks/usePageTitle';
+
+const Reviews: FC = () => {
+	usePageTitle('reviews');
+
+	const [reviews, setReviews] = useState<Review[]>([]);
+	const user = useLoggedInUser();
+
+	useEffect(
+		() =>
+			onSnapshot(reviewsCollection, snapshot => {
+				setReviews(snapshot.docs.map(doc => doc.data()));
+			}),
+		[]
+	);
+
+	return (
+		<>
+			<Box
+				sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+			>
+				<Typography variant="h4">User reviews:</Typography>
+				{/* Limit to one review per user */}
+				{!reviews.find(r => r.by === user?.email) && (
+					<AddReview>
+						{open => (
+							<Button onClick={open} variant="contained">
+								Add a review
+							</Button>
+						)}
+					</AddReview>
+				)}
+			</Box>
+			{reviews.map((r, i) => (
+				<ReviewPreview key={i} {...r} />
+			))}
+		</>
+	);
+};
+
 export default Reviews;
