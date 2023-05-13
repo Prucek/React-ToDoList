fc9e581867250b4dd7e291dca1bec5ece79f3b74
Marek Miček <marek.micek@azet.sk>
2023-05-13 22:05:04 +0200

    Show only categories of login user and default one

--- src/components/CategoryGrid.tsx
+++ src/components/CategoryGrid.tsx
@@ -2,6 +2,7 @@ import { Button, Grid } from '@mui/material';
 import { FC } from 'react';
 
 import { Category } from '../firebase';
+import useLoggedInUser from '../hooks/useLoggedInUser';
 
 import AddCategory from './AddCategory';
 import EditCategory from './EditCategory';
@@ -11,34 +12,48 @@ type Props = {
 	categories: Category[];
 };
 
-const CategoryGrid: FC<Props> = ({ categories }) => (
-	<Grid
-		container
-		spacing={{ xs: 2, md: 3, lg: 4 }}
-		columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
-	>
-		{categories.map((category, i) => (
-			<EditCategory key={i} category={category}>
-				{open => <CategoryPreview key={i} category={category} onClick={open} />}
-			</EditCategory>
-		))}
-		<Grid item xs={2} sm={4} md={4} lg={4}>
-			<AddCategory>
-				{open => (
-					<Button
-						onClick={open}
-						variant="contained"
-						sx={{
-							aspectRatio: '1',
-							width: '100%'
-						}}
-					>
-						Add Category
-					</Button>
-				)}
-			</AddCategory>
+const CategoryGrid: FC<Props> = ({ categories }) => {
+	const user = useLoggedInUser();
+
+	return (
+		<Grid
+			container
+			spacing={{ xs: 2, md: 3, lg: 4 }}
+			columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
+		>
+			{categories.map((category, i) => {
+				// show only categories of login user and default one
+				if (
+					category.email === user?.email ||
+					category.email === 'build_in_category'
+				) {
+					return (
+						<EditCategory key={i} category={category}>
+							{open => (
+								<CategoryPreview key={i} category={category} onClick={open} />
+							)}
+						</EditCategory>
+					);
+				}
+			})}
+			<Grid item xs={2} sm={4} md={4} lg={4}>
+				<AddCategory>
+					{open => (
+						<Button
+							onClick={open}
+							variant="contained"
+							sx={{
+								aspectRatio: '1',
+								width: '100%'
+							}}
+						>
+							Add Category
+						</Button>
+					)}
+				</AddCategory>
+			</Grid>
 		</Grid>
-	</Grid>
-);
+	);
+};
 
 export default CategoryGrid;
