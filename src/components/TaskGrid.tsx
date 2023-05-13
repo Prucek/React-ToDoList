dc7d1c917443afff433da1a0d1ad0e1af0765b3a
Marek Miček <marek.micek@azet.sk>
2023-05-13 22:04:11 +0200

    Show only tasks of login user

--- src/components/TaskGrid.tsx
+++ src/components/TaskGrid.tsx
@@ -2,6 +2,7 @@ import { Button, Grid } from '@mui/material';
 import { FC } from 'react';
 
 import { Task } from '../firebase';
+import useLoggedInUser from '../hooks/useLoggedInUser';
 
 import TaskPreview from './TaskPreview';
 import AddTask from './AddTask';
@@ -12,7 +13,7 @@ type Props = {
 };
 
 const TaskGrid: FC<Props> = ({ tasks }) => {
-	const mpty = 0;
+	const user = useLoggedInUser();
 
 	return (
 		<Grid
@@ -20,11 +21,16 @@ const TaskGrid: FC<Props> = ({ tasks }) => {
 			spacing={{ xs: 2, md: 3, lg: 4 }}
 			columns={{ xs: 4, sm: 8, md: 12, lg: 16 }}
 		>
-			{tasks.map((task, i) => (
-				<EditTask key={i} task={task}>
-					{open => <TaskPreview key={i} task={task} onClick={open} />}
-				</EditTask>
-			))}
+			{tasks.map((task, i) => {
+				// show only tasks of login user
+				if (task.email === user?.email) {
+					return (
+						<EditTask key={i} task={task}>
+							{open => <TaskPreview key={i} task={task} onClick={open} />}
+						</EditTask>
+					);
+				}
+			})}
 			<Grid item xs={2} sm={4} md={4} lg={4}>
 				<AddTask>
 					{open => (
