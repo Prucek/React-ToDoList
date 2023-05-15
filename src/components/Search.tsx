c05a0171660f335e20d9c206ba7fee3751706e07
Prucek <prucek@redhat.com>
2023-05-15 14:11:23 +0200

    Removed unnecessary classname

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,7 +10,7 @@ export const Search = ({
 	<TextField
 		label="Search"
 		className="w-full px-4 py-2 text-gray-700 rounded border-2 focus:outline-none
-         focus:ring-2 focus:ring-purple-800 mt-4 input flex-1"
+         focus:ring-2 focus:ring-purple-800 input flex-1"
 		type="text"
 		onChange={onChange}
 		placeholder="Search by name ..."
